c7b944ac5e289ef6aed5881e50630044f36373df
lemecs <d090445@gmail.com>
2025-02-14 00:29:57 +0300

    a

--- laba_1/rab1.cpp
+++ laba_1/rab1.cpp
@@ -1,12 +1,64 @@
+//rab1
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 int main()
 {
 	printf("enter radius and height\n");
-	float rad = 0.0, hei = 0.0;
-	scanf("%f%f", &rad, &hei);
-	float v = 3.14 * rad * rad * hei;
-	printf("Vcylinder = %f\n", v);
+	double rad = 0.0, hei = 0.0;
+	scanf("%lf%lf", &rad, &hei);
+	printf("vcylinder = %.2lf\n", (double) 3.14 * rad * rad * hei);
 	return 0;
 }
+
+////rab2
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	printf("enter U and R\n");
+//	double u = 0.0, r = 0.0;
+//	scanf("%lf%lf", &u, &r);
+//	printf("Answer: %.2lf", (double) u/r);
+//	return 0;
+//}
+//
+//rab3
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	printf("enter kg cost and weight\n");
+//	double c = 0.0, w = 0.0;
+//	scanf("%lf%lf", &c, &w);
+//	printf("final price is %.2lf", (double) c*w);
+//	return 0;
+//}
+//
+//rab4
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	printf("enter weight in lb\n");
+//	double w;
+//	scanf("%lf", &w);
+//	printf("weight in kg: %.2lf", (double)(w * 405.9) / 1000);
+//	return 0;
+//}
+
+//rab5
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	printf("enter price\n");
+//	double v;
+//	scanf("%lf", &v);
+//	printf("final price is %.2lf", (double) v/10);
+//	return 0;
+//}
